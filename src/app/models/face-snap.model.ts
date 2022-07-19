5c8f88b176f0580cc32cd26515cfc3059df822a4
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-07-19 16:59:23 +0200

    Creating FaceSnap model

---
+++ src/app/models/face-snap.model.ts
@@ -0,0 +1,9 @@
+export class FaceSnap {
+  constructor(
+    public imageUrl: string,
+    public title: string,
+    public description: string,
+    public snaps: number,
+    public createdDate: Date
+  ) {}
+}
