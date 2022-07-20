e42916b230b911ddb201b587de30c7ad6de7a356
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-07-20 14:12:02 +0200

    Switch faceSnap instances from individual properties to array of instances and added iteration in html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,36 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  firstSnap!: FaceSnap;
-  secondSnap!: FaceSnap;
-  thirdSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.firstSnap = {
-      imageUrl: 'https://images.unsplash.com/photo-1564499504739-bc4fc2ae8cba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2833&q=80',
-      title: 'Kitesurf',
-      description: 'Mon sport préféré',
-      snaps: 6,
-      createdDate: new Date(),
-      location: "Sardaigne"
-    };
-    this.secondSnap = {
-      imageUrl: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-      title: 'Mountains',
-      description: 'Mon lieu favoris',
-      snaps: 8,
-      createdDate: new Date(),
-      location: "Alpes Françaises"
-    };
-    this.thirdSnap = {
-      imageUrl: 'https://images.unsplash.com/photo-1492691527719-9d1e07e534b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80',
-      title: 'Photography',
-      description: 'Mon activité préférée',
-      snaps: 2,
-      createdDate: new Date()
-    };
+    this.faceSnaps = [
+      {
+        imageUrl:
+          'https://images.unsplash.com/photo-1564499504739-bc4fc2ae8cba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2833&q=80',
+        title: 'Kitesurf',
+        description: 'Mon sport préféré',
+        snaps: 6,
+        createdDate: new Date(),
+        location: 'Sardaigne',
+      },
+      {
+        imageUrl:
+          'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+        title: 'Mountains',
+        description: 'Mon lieu favoris',
+        snaps: 8,
+        createdDate: new Date(),
+        location: 'Alpes Françaises',
+      },
+      {
+        imageUrl:
+          'https://images.unsplash.com/photo-1492691527719-9d1e07e534b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80',
+        title: 'Photography',
+        description: 'Mon activité préférée',
+        snaps: 2,
+        createdDate: new Date(),
+      },
+    ];
   }
 }
