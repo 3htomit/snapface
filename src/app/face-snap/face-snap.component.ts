8394995aa43e88f8630811ecc66211560737f696
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-07-22 12:43:56 +0200

    Refactored the onSnap method

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap',
@@ -10,17 +11,14 @@ export class FaceSnapComponent implements OnInit {
   @Input() faceSnap!: FaceSnap;
   snapped!: boolean;
 
+  constructor(private faceSnapsService: FaceSnapsService) {}
+
   ngOnInit() {
     this.snapped = false;
   }
 
   onSnap() {
-    if (this.snapped) {
-      this.faceSnap.snaps--;
-      this.snapped = false;
-    } else {
-      this.faceSnap.snaps++;
-      this.snapped = true;
-    }
+    this.faceSnapsService.snappingFaceSnapById(this.faceSnap.id, this.snapped);
+    this.snapped = !this.snapped;
   }
 }
